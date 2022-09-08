e7a865b72ac0ea58b4fd8dbf25b11d43571105d7
Manaball123 <mana123__@hotmail.com>
2022-09-08 11:16:05 +0800

    this should work if !china

--- scraper-main.py
+++ scraper-main.py
@@ -23,48 +23,40 @@ dir = "./anime-porn/"
 
 cf = r"pKC9inHoylW0t4Ip4QecCiw7P6g0xmGWXjjRWWJNiKY-1653062338-0-150"
 headers = {
-        "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0",
+    "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0",
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers',
+}
+
+proxy = {
+    "https" : "https://127.0.0.1:7890"
 }
 
 posturl = "https://danbooru.donmai.us/posts/"
 def get_request(page):
     url_paged = url + "&page=" + str(page)
-    resp = requests.get(url=url_paged)
+    resp = requests.get(url = url_paged)
 
     data = resp.json()
     
     res = []
     for i in range(len(data)):
-        if data[i].__contains__("id"):
-            res.append(data[i]["id"])
+        
+            res.append({
+                "tid" : data[i]["id"],
+                "url" : data[i]["large_file_url"]
+            })
+        
+            
     
 
     return res
     
 
     
-
-def find_cdn_url_from_resp(respc):
-    pattern = "https://cdn.donmai.us/original"
-    index = respc.find(pattern)
-    
-
-    encounters = 0
-    url = ""
-    for i in range(index, index + 200):
-        
-        if(respc[i] == "\""):
-            #print(url)
-            return url
-        url += respc[i]
-
-    return None
-
-def get_cdn_url(id):
-    url = posturl + str(id)
-    response = requests.get(url, headers=headers)
-
-    return find_cdn_url_from_resp(str(response.content))
 
 def find_ext(str):
 
@@ -80,19 +72,17 @@ def find_ext(str):
     return None
 
 
-def save_image(id):
+def save_image(task):
 
-    url = get_cdn_url(id)
-    if(url == None):
-        return
-    ext = find_ext(url)
+
+    ext = find_ext(task["url"])
     if(ext == None):
         return 
-    sdir = dir + str(id % 100) + "/"
-    name = str(id) + ext
+    sdir = dir + str(task["tid"] % 100) + "/"
+    name = str(task["tid"]) + ext
     if(os.path.exists(sdir + name)):
         print(name + " Exists")
-    res = requests.get(url=url,headers=headers, stream=True)
+    res = requests.get(url=task["url"],headers=headers, stream=True)
     
     #print("Retrieving from " + url)
     if res.status_code == 200:
@@ -110,8 +100,8 @@ def mkdir():
 
 #makes the anime porn folder
 def mkrootdir():
-    if(os.path.isdir(dir)):
-        os.mkdir(dir)
+    if(not os.path.isdir("anime-porn")):
+        os.mkdir("anime-porn")
 
 
 
@@ -125,12 +115,12 @@ if __name__ ==  "__main__":
     pool = ThreadPoolExecutor(max_workers = threads)
     
     for i in page_range:
-        resp = get_request(i)
+        tasks = get_request(i)
 
 
         print("Starting downloads for page " + str(i))
         print("\n\n")
-        pool.map(save_image, resp)
+        pool.map(save_image, tasks)
         print("\n\n")
 
         print("Finished downloading page " + str(i))
